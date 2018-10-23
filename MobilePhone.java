e10d3ef705ee1e34ddbb8751a645f510d5933497
VicChern <32546828+VicChern@users.noreply.github.com>
2018-10-23 18:04:23 +0300

    Update MobilePhone.java

--- MobilePhone.java
+++ MobilePhone.java
@@ -1,17 +1,27 @@
 package com.company.MobilePhone;
+
 /**
  * this app able to do some operations with data, as simulation
  * of contacts menu in mobile phone
+ *
  * @author Vic Chern
  * @version 1.01
- */
-
-/**
+ * <p>
  * Not decided problems:
  * In this case usersInput normally working with next() method only
  * In next contacts, your phone number should contains at most the same number of digits that contains first contact
  * or would be thrown Mismatch Exception.
  * What about the size of com.company.MobilePhone.MobilePhone class? Should I separate it to more class. Or size is OK?
+ */
+
+/*
+ * Not decided problems:
+ * In this case usersInput normally working with next() method only
+ * In next contacts, your phone number should contains
+ * at most the same number of digits that contains first contact
+ * or would be thrown Mismatch Exception.
+ * What about the size of com.company.MobilePhone.MobilePhone class? Should I separate it to more class.
+ * Or size is OK?
  */
 
 import java.util.HashMap;
@@ -25,7 +35,7 @@ public class MobilePhone {
     private HashMap<String, Integer> contacts = new HashMap<>();
     private final String ASK = "You want to continue?";
 
-    public void listOfContacts() {
+    private void listOfContacts() {
         for (String c : contacts.keySet()) {
             Integer value = contacts.get(c);
             System.out.println(c + ": " + value + "\n");
@@ -40,7 +50,7 @@ public class MobilePhone {
 
     }
 
-    public void addNewContact() throws InputMismatchException {
+    private void addNewContact() throws InputMismatchException {
         try {
             System.out.println("Please, enter name and phone number");
             name = usersInput.next();
@@ -52,14 +62,14 @@ public class MobilePhone {
         } catch (InputMismatchException e) {
             System.out.println("Invalid enter please repeat \n");
             addNewContact();
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         menu();
 
     }
 
-    public void removeContact() {
+    private void removeContact() {
         System.out.println("Please enter contact, that you want to remove: ");
         name = usersInput.next();
         if (contacts.containsKey(name)) {
@@ -73,14 +83,21 @@ public class MobilePhone {
 
     }
 
-    public void menu() {
-        System.out.println("1. Print list of contacts \n" +
-                "2. Add new contact \n" +
-                "3. Update existing contact \n" +
-                "4. Remove contact \n" +
-                "5. Search/find contact \n" +
-                "6. Exit");
-        int point = usersInput.nextInt();
+    public final void menu() {
+        System.out.println(
+                "1. Print list of contacts \n"
+                        + "2. Add new contact \n"
+                        + "3. Update existing contact \n"
+                        + "4. Remove contact \n"
+                        + "5. Search/find contact \n"
+                        + "6. Exit");
+
+        int point = 0;
+        try {
+            point = usersInput.nextInt();
+        } catch (Exception e) {
+            System.out.println("Only numbers accepted, please repeat");
+        }
         switch (point) {
             case 1:
                 listOfContacts();
@@ -89,7 +106,7 @@ public class MobilePhone {
                 addNewContact();
                 break;
             case 3:
-                updateExistingContact(name, phoneNumber);
+                updateExistingContact();
                 break;
             case 4:
                 removeContact();
@@ -99,32 +116,39 @@ public class MobilePhone {
                 break;
             case 6:
                 System.out.println("You just exit from your program");
+                break;
+                default:
+                    System.out.println("Wrong enter. Please repeat \n");
+                    menu();
         }
     }
 
-    public void updateExistingContact(String UpdatedName, int updatedPhoneNumber) {
+    private void updateExistingContact() {
+        String updatedName;
+        int updatedPhoneNumber;
+
         System.out.println("Please enter contact, that you want to replace: ");
         if (contacts.containsKey(usersInput.next())) {
 
             System.out.println("Enter new name of contact");
-            UpdatedName = usersInput.next();
+            updatedName = usersInput.next();
 
             System.out.println("Enter new phone number");
             updatedPhoneNumber = usersInput.nextInt();
 
             contacts.replace(name, phoneNumber, updatedPhoneNumber);
-            contacts.put(UpdatedName, contacts.remove(name));
+            contacts.put(updatedName, contacts.remove(name));
 
             System.out.println("\nYour contact list was updated \n" + ASK);
             askMethod();
-        } else
+        } else {
             System.out.println("This contact doesn't exist. Please repeat \n");
 
-        menu();
-
+            menu();
+        }
     }
 
-    public void searchFind() {
+    public final void searchFind() {
         System.out.println("Enter name of contact that you want to find:");
         name = usersInput.next();
         if (contacts.containsKey(name)) {
@@ -138,7 +162,7 @@ public class MobilePhone {
         menu();
     }
 
-    public void askMethod() {
+   private void askMethod() {
         System.out.println("Yes or No?");
         String answer;
         answer = usersInput.next();
